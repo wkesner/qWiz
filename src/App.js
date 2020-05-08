398ed4c911303eee07c1b058af5e4990f0f58535
wkesner <wkesner495@gmail.com>
2020-05-08 11:34:49 -0700

    add response buttons

--- src/App.js
+++ src/App.js
@@ -8,9 +8,41 @@ function App() {
       <header className="App-header">
           <h1>{activeQuestion}
           </h1>
+          <div className="responseButtons">
+            <button>{ questionBank[questionCounter].responseA }</button>
+            <button>{ questionBank[questionCounter].responseB }</button>
+            <button>{ questionBank[questionCounter].responseC }</button>
+            <button>{ questionBank[questionCounter].responseD }</button>
+          </div>
       </header>
     </div>
   );
 }
-export const activeQuestion = 'Do girls just wanna have fun?';
+
+export const activeResponses = {
+  responseA: 'yes',
+  responseB: 'no',
+  responseC: 'maybe so',
+  responseD: 'response D'
+};
+
+
+
+const questionBank = [
+  {question: 'Do girls just wanna have fun?',
+  responseA: 'yes',
+  responseB: 'no',
+  responseC: 'maybe so',
+  responseD: 'response D'},
+
+  {question: 'Is it raining men?',
+  responseA: 'halleujah',
+  responseB: 'yes',
+  responseC: 'no',
+  responseD: 'maybe so'}
+]
+
+
+let questionCounter = Math.floor(Math.random() * Math.floor(questionBank.length));
+export const activeQuestion = questionBank[questionCounter].question;
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+
 import { activeQuestion } from './App';
+import { activeResponses } from './App';
+
 
 const assert = require('chai').assert;
 
 
 describe('Question', () => {
   it('displays the correct question', () => {
-    const expectation = 'Do girls just wanna have fun?';
+
+    let expectation = '';
+    let i=0;
+
+    if (i===0) {
+      expectation = 'Do girls just wanna have fun?';
+    } else if(i===1) {
+      expectation = 'Is it raining men?'
+    }
+
     const actual = activeQuestion;
+    assert.equal(actual, expectation);
+  })
+})
+
+describe('Responses', () => {
+  it("displays the correct 'a' response", () => {
+    const expectation = 'yes';
+    const actual = activeResponses.responseA;
 
     assert.equal(actual, expectation);
   })
