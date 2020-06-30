3779c675435b5f50fd8ff5f1d6ad07d5f2f07402
root <wkesner495@gmail.com>
2020-06-30 15:03:17 -0700

    capturing input with state to question object, have not yet implemented adding quiz to quizArray

--- src/components/Quizmaker.js
+++ src/components/Quizmaker.js
@@ -34,32 +34,83 @@ const logQuestion = id => {
 
 export class Quizmaker extends React.Component {
   constructor(props) {
-    super(props)
-
+    super(props);
+    this.state = {
+      questionValue: ' ',
+      aValue: ' ',
+      bValue: ' ',
+      cValue: ' ',
+      dValue: ' ',
+      correctValue: ' ',
+      resourceValue: ' ',
+    };
   }
 
+  handleQuestionChange = event => {
+    this.setState({ questionValue: event.target.value}); //assigning value from dom
+    questionObject.question = this.state.questionValue;
+    console.log("A question was logged: " + questionObject.question);
+  }
+
+  handleAChange = event => {
+    this.setState({ aValue: event.target.value});
+    questionObject.aValue = this.state.aValue;
+    console.log("AValue was logged: " + questionObject.aValue);
+  }
+
+  handleBChange = event => {
+    this.setState({ bValue: event.target.value});
+    questionObject.bValue = this.state.bValue;
+    console.log("BValue was logged: " + questionObject.bValue);
+  }
+
+  handleCChange = event => {
+    this.setState({ cValue: event.target.value});
+    questionObject.cValue = this.state.cValue;
+    console.log("cValue was logged: " + questionObject.cValue);
+  }
+
+
   render() {
+
     return(
       <div className='Quizmaker'>
         <h1> MAKE YOUR QUIZ </h1>
-          <div>
+          <form>
             <label>Question:
-              <input type="text" id="Question" value=' ' onChange={logQuestion('Question')}/>
-              <script>
-                {console.log(questionObject.question)}
-              </script>
+              <input
+              type="text"
+              id="Question"
+              placeholder="Enter your question here."
+              value={this.state.questionValue}
+              onChange={this.handleQuestionChange} />
             </label>
               <br></br>
             <label>Response A:
-              <input type="text" id="Response A"/>
+              <input
+              type="text"
+              id="Response A"
+              placeholder="Enter your first response here."
+              value={this.state.aValue}
+              onChange={this.handleAChange} />
             </label>
               <br></br>
             <label>Response B:
-              <input type="text" id="Response B"/>
+              <input
+              type="text"
+              id="Response B"
+              placeholder= "Enter your second response here."
+              value={this.state.bValue}
+              onChange={this.handleBChange} />
             </label>
               <br></br>
             <label>Response C:
-              <input type="text" id="Response C"/>
+              <input
+              type="text"
+              id="Response C"
+              placeholder="Enter your third response here."
+              value={this.state.cValue}
+              onChange={this.handleCChange} />
             </label>
               <br></br>
             <label>Response D:
@@ -76,7 +127,7 @@ export class Quizmaker extends React.Component {
               <br></br>
             <button>Add Question</button>
             <button>Finish Quiz</button>
-          </div>
+          </form>
       </div>
 
     )
